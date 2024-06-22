c5a2fcfe5e73975c447501e0b1526c7df45f470c
josephburgess <josephburgess@gmail.com>
2024-06-23 00:27:22 +0200

    nvim updates

--- .config/nvim/lua/plugins/hardtime.lua
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -2,5 +2,13 @@ return {
   "m4xshen/hardtime.nvim",
   event = "VeryLazy",
   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  opts = {},
+  opts = {
+    max_count = 5,
+    disabled_keys = {
+      ["<Up>"] = {},
+      ["<Down>"] = {},
+      ["<Left>"] = {},
+      ["<Right>"] = {},
+    },
+  },
 }
