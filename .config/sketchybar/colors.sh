80f39178cd21ac2322d87df6b00e43f84d9b1118
josephburgess <josephburgess@gmail.com>
2024-06-22 09:04:45 +0200

    sketchybar

---
+++ .config/sketchybar/colors.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export PURPLE=0xFF303840      # Replaced with a darker grey
+export GREY=0x222436FF        # Kept as is, already a dark grey
+export TRANSPARENT=0x00000000 # Kept as is, transparent
+export WHITE=0xFFFFFFFF       # Kept as is, white
+export BLUE=0x292E42FF        # Kept as is, dark blue
+export MAGENTA=0xFF007CFF     # Replaced with a dark grey-blue
+export ORANGE=0xFF966CFF      # Replaced with a mid-grey
+export TEMPUS=0xFF1E2030      # Kept as is, dark grey
+export STATUS=0xFF394B70      # Kept as is, dark grey-blue
+export SPACEBG=0xAA303840     # Replaced with a semi-transparent dark grey
+export MIDNIGHT=0xFF3D59A1    # Kept as is, dark blue
