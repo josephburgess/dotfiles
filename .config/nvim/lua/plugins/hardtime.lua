bb8bf642c38c1df5efef5a0d52e959acae89f278
josephburgess <josephburgess@gmail.com>
2024-10-08 16:41:51 +0100

    remove hardtime etc, we arent babies any more

--- .config/nvim/lua/plugins/hardtime.lua
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -1,24 +1,24 @@
 return {
-  "m4xshen/hardtime.nvim",
-  event = "VeryLazy",
-  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  opts = {
-    max_count = 9,
-    disabled_keys = {
-      ["<Up>"] = {},
-      ["<Down>"] = {},
-      ["<Left>"] = {},
-      ["<Right>"] = {},
-    },
-    disable_mouse = false,
-    allow_different_key = true,
-    hints = {
-      ["[dcyvV][ia][%(%)]"] = {
-        message = function(keys)
-          return "Use " .. keys:sub(1, 2) .. "b instead of " .. keys
-        end,
-        length = 3,
-      },
-    },
-  },
+  -- "m4xshen/hardtime.nvim",
+  -- event = "VeryLazy",
+  -- dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  -- opts = {
+  --   max_count = 9,
+  --   disabled_keys = {
+  --     ["<Up>"] = {},
+  --     ["<Down>"] = {},
+  --     ["<Left>"] = {},
+  --     ["<Right>"] = {},
+  --   },
+  --   disable_mouse = false,
+  --   allow_different_key = true,
+  --   hints = {
+  --     ["[dcyvV][ia][%(%)]"] = {
+  --       message = function(keys)
+  --         return "Use " .. keys:sub(1, 2) .. "b instead of " .. keys
+  --       end,
+  --       length = 3,
+  --     },
+  --   },
+  -- },
 }
