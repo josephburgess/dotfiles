8f5526d40b4b447fe2a5ca2103a701806f87af23
josephburgess <josephburgess@gmail.com>
2024-12-18 11:20:57 +0000

    Update karabiner config; fix broken lualine

--- karabiner-config/src/index.ts
+++ karabiner-config/src/index.ts
@@ -28,10 +28,7 @@ writeToProfile("karabiner-config", [
   // Supported alias: https://github.com/evan-liu/karabiner.ts/blob/main/src/utils/key-alias.ts
 
   layer("/", "symbol-mode").manipulators([
-    // //     / + [ 1    2    3    4    5 ] =>
-    // withMapper(["⌘", "⌥", "⌃", "⇧", "⇪"])((k, i) =>
-    //   map((i + 1) as NumberKeyValue).toPaste(k)
-    // ),
+    // Example of how to paste common characters on this layer
     // withMapper(["←", "→", "↑", "↓", "␣", "⏎", "⇥", "⎋", "⌫", "⌦", "⇪"])((k) =>
     //   map(k).toPaste(k)
     // ),
@@ -39,6 +36,15 @@ writeToProfile("karabiner-config", [
 
   rule("Caps Lock -> Hyper").manipulators([
     map("caps_lock").toHyper().toIfAlone("escape"),
+    map("right_option").toMeh(),
+    map("right_command").toHyper(),
+  ]),
+
+  rule("Hyper + A/D for Tab Navigation").manipulators([
+    withModifier("Hyper")({
+      s: toKey("tab", ["left_control", "shift"]),
+      d: toKey("tab", ["left_control"]),
+    }),
   ]),
 
   rule("Tab -> Meh").manipulators([map("tab").toMeh().toIfAlone("tab")]),
@@ -50,43 +56,15 @@ writeToProfile("karabiner-config", [
     ),
   ),
 
-  rule("Launch Apps").manipulators([
-    withModifier("Hyper")({
-      f: toApp("Firefox Developer Edition"),
-      s: toApp("Slack"),
-      ";": toApp("Kitty"),
-    }),
-  ]),
-
   rule("Control").manipulators([
     withModifier("Hyper")({
-      // h: toKey("rewind"),
-      // l: toKey("fastforward"),
-      // k: toKey("play_or_pause"),
-      // j: toKey("volume_decrement"),
-      // u: toKey("volume_increment"),
-      // m: toKey("mute"),
       i: toKey("apple_display_brightness_decrement"),
       p: toKey("apple_display_brightness_increment"),
     }),
   ]),
 
-  hyperLayer("b", "browser-launch-layer").manipulators([
-    { g: to$('open "https://github.com"') },
-    { y: to$('open "https://youtube.com"') },
-    { c: to$('open "https://app.fastmail.com/calendar/b"') },
-    { m: to$('open "https://fastmail.com"') },
-    { n: to$('open "https://notion.so"') },
-    { r: to$('open "https://news.ycombinator.com"') },
-    {
-      j: to$(
-        'open "https://nestiolistings.atlassian.net/jira/software/projects/AUTO/boards/78?assignee=712020%3Aee6769de-6645-4366-a7c5-75deffd3351a"',
-      ),
-    },
-  ]),
-
-  rule("Key mapping").manipulators([
-    // config key mappings
-    map(1).to(1),
-  ]),
+  // hyperLayer("b", "browser-launch-layer").manipulators([
+  //   { g: to$('open "https://github.com"') },
+  //   { y: to$('open "https://youtube.com"') },
+  // ]),
 ]);
