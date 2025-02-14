658f0626ebbd72e88a9de26fba3b7924d2efcc80
josephburgess <josephburgess@gmail.com>
2025-02-14 14:10:37 -0300

    Nvumi / blink

---
+++ .config/nvim/lua/plugins/nvumi.lua
@@ -0,0 +1,12 @@
+return {
+  "nvumi",
+  dir = "~/Projects/nvumi/",
+  dependencies = { "folke/snacks.nvim" },
+  opts = { virtual_text = "newline" },
+  -- opts = { virtual_text = "inline" },
+}
+-- return {
+--   "josephburgess/nvumi",
+--   dependencies = { "folke/snacks.nvim" },
+--   opts = {},
+-- }
